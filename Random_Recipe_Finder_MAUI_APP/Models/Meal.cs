1a7163b7fe4953cf9111b54fd6bf2362cea34e32
Rayquation <emil372r@gmail.com>
2025-02-19 09:16:57 +0100

    Add Meal class to represent meal attributes
    Introduces a new `Meal` class in the `Random_Recipe_Finder_MAUI_APP.Models` namespace. The class includes properties for meal details such as ID, name, category, area, instructions, thumbnail, tags, YouTube link, source, image source, Creative Commons confirmation, and date modified. It also features two lists for ingredients and measures, each initialized to hold up to 20 items.

---
+++ Random_Recipe_Finder_MAUI_APP/Models/Meal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Random_Recipe_Finder_MAUI_APP.Models
+{
+    public class Meal
+    {
+        public string IdMeal { get; set; }
+        public string StrMeal { get; set; }
+        public string StrDrinkAlternate { get; set; }
+        public string StrCategory { get; set; }
+        public string StrArea { get; set; }
+        public string StrInstructions { get; set; }
+        public string StrMealThumb { get; set; }
+        public string StrTags { get; set; }
+        public string StrYoutube { get; set; }
+        public List<string> Ingredients { get; set; } = new List<string>(new string[20]);
+        public List<string> Measures { get; set; } = new List<string>(new string[20]);
+        public string StrSource { get; set; }
+        public string StrImageSource { get; set; }
+        public string StrCreativeCommonsConfirmed { get; set; }
+        public string DateModified { get; set; }
+    }
+}
