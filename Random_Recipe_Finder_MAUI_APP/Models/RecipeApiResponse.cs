3e3572ff4738c971661bf09d2f15ae494d2ff3d8
Aksel Fabricius <akselfabricius@gmail.com>
2025-02-19 09:39:58 +0100

    Add RecipeApiResponse and RecipeApiService classes
    Introduce RecipeApiResponse to hold a list of Recipe objects. Implement RecipeApiService for making HTTP requests to an external API to search for recipes by name. Fix a syntax error in the SearchRecipeByName method to return an empty list correctly.

---
+++ Random_Recipe_Finder_MAUI_APP/Models/RecipeApiResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Random_Recipe_Finder_MAUI_APP.Models
+{
+    class RecipeApiResponse
+    {
+        public List<Recipe> Meals { get; set; }
+    }
+}
---
+++ Random_Recipe_Finder_MAUI_APP/Services/RecipeApiService.cs
@@ -0,0 +1,44 @@
+﻿using Random_Recipe_Finder_MAUI_APP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Random_Recipe_Finder_MAUI_APP.Services
+{
+    class RecipeApiService
+    {
+        private static readonly string MAIN_URL_ENPOINT = "www.themealdb.com/api/json/v1/1/";
+        
+        private HttpClient client;
+        public RecipeApiService()
+        {
+            Uri uri = new Uri(MAIN_URL_ENPOINT);
+            client = new HttpClient() { BaseAddress = uri};
+        }
+
+        public async Task<List<Recipe>> SearchRecipeByName(string recipeName)
+        {
+
+            string API_ENPOINT = $"search.php?f={recipeName}";
+            
+            HttpResponseMessage response = await client.GetAsync(API_ENPOINT);
+
+            if (response.IsSuccessStatusCode)
+            {
+
+                RecipeApiResponse recipeApiResponse = await response.Content.ReadFromJsonAsync<RecipeApiResponse>();
+
+                if (recipeApiResponse != null)
+                {
+                    return recipeApiResponse.Meals;
+                }
+            }
+
+            return [];
+        }
+
+    }
+}
