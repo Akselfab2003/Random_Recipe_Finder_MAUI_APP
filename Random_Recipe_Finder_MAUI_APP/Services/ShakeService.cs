5e0f0c33ca05e3fd8e9e3b4cfcfb377653e1d6ce
Aksel Fabricius <akselfabricius@gmail.com>
2025-02-20 07:56:55 +0100

    Add shake detection feature to the application
    This commit introduces a new shake detection feature.
    The `IShakeService` is injected in `Home.razor`, initializing
    shake detection in the `OnInitialized` method. The service
    is started if not already running, and it subscribes to
    shake events, logging a message when a shake is detected.
    
    Additionally, `IShakeService` is registered in
    `MauiProgram.cs`, and its interface is defined in
    `IShakeService.cs`. The `ShakeService.cs` file implements
    the interface, utilizing the accelerometer to manage
    shake detection and service state.

---
+++ Random_Recipe_Finder_MAUI_APP/Services/ShakeService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Random_Recipe_Finder_MAUI_APP.Services
+{
+    public class ShakeService: IShakeService
+    {
+        private readonly IAccelerometer _accelerometer;
+        private bool _isShakeServiceRunning = false;
+        public ShakeService()
+        {
+            _accelerometer = Accelerometer.Default;
+        }
+
+        public bool IsRunning()
+        {
+            return _isShakeServiceRunning;
+        }
+
+        public void StartShakeService()
+        {
+            _accelerometer.Start(SensorSpeed.Default);
+            _isShakeServiceRunning = true;
+        }
+
+        public void StopShakeService()
+        {
+            _accelerometer.Stop();
+        }
+
+        public void SubscribeToShakeEvent(EventHandler action)
+        {
+            _accelerometer.ShakeDetected += action;
+        }
+
+    }
+}
